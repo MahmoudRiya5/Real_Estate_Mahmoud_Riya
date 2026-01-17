04e86debd47147bfdda58b223985f8e9aa747021
Lama Soliman <lama.soliman2023@gmail.com>
2026-01-18 00:23:40 +0100

    إصلاح التداخل وتحديث الألوان والخطوط

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,23 @@ import AboutUs from "./pages/AboutUs"
 import Properties from "./pages/Properties"
 import Services from "./pages/Services"
 import ContactUs from "./pages/ContactUs"
+import NavBar from "./componant/NavBar/NavBar"
+import StartSection from './componant/StartSection/StartSection';
+import Footer from './componant/Footer/Footer';
+
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/about" element={<AboutUs/>} />
-        <Route path="/properties" element={<Properties/>} />
-        <Route path="/services" element={<Services/>} />
-        <Route path="/contact" element={<ContactUs/>} />
-      </Routes>
+    <NavBar />
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/about" element={<AboutUs/>} />
+      <Route path="/properties" element={<Properties/>} />
+      <Route path="/services" element={<Services/>} />
+      <Route path="/contact" element={<ContactUs/>} />
+    </Routes>
+      <StartSection />
+      <Footer />
     </>
   )
 }
---
+++ src/componant/StartSection/StartSection.jsx
@@ -0,0 +1,17 @@
+import './StartSection.css';
+
+const StartSection = () => {
+    return (
+        <section className="LS-start-container">
+            <div className="LS-start-text-group">
+                <h2 className="LS-start-title section-title">Start Your Real Estate Journey Today</h2>
+                <p className="LS-start-description body">
+                    Your dream property is just a click away. Whether you're looking for a new home, a strategic investment, or expert real estate advice, Estatein is here to assist you every step of the way. Take the first step towards your real estate goals and explore our available properties or get in touch with our team for personalized assistance.
+                </p>
+            </div>
+            <button className="LS-start-button button ">Explore Properties</button>
+            </section>
+    );
+};
+
+export default StartSection;  
