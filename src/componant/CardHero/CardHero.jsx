59ef93a2728ccf40d4277f51e881784ea4813316
hala ali <hala3106272@gmail.com>
2026-01-19 03:09:41 +0300

    fix: responsive cards and hero section styles

---
+++ src/componant/CardHero/CardHero.jsx
@@ -0,0 +1,23 @@
+import './CardHero.css'
+export const CardHero = ({ title, icon, arrowIcon }) => {
+    return (
+        <div className="card-hero-container">
+
+            <div className="arrow-wrapper">
+                <img src={arrowIcon} alt="arrow" className="arrow-icon" />
+            </div>
+
+
+            <div className="icon-outer-ring">
+                <div className="icon-inner-circle">
+                    <img src={icon} alt={title} className="card-main-icon" />
+                </div>
+            </div>
+
+
+            <h3 className="card-title">{title}</h3>
+        </div>
+    );
+};
+
+export default CardHero;
