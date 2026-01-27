fd92749539c4c2a1dbe885e8927a6cca19767ab2
Lama Soliman <lama.soliman2023@gmail.com>
2026-01-27 14:19:01 +0100

    Resolve conflicts

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,9 +1,9 @@
-
+import React from 'react'
+import LetConnect  from '../componant/LetConnect/LetConnect'
 const ContactUs = () => {
   return (
     <>
-    
-      
+      <LetConnect />
       hello from contact us
     </>
   )
